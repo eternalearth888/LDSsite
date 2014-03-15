e85123a09700a223cf15bebcdb053a7d191568c4
Maria Deslis <mdeslis@gmail.com>
2014-03-15 13:50:18 -0600

    made arrays seperate files

---
+++ sqlFiles/submissions_db.sql
@@ -0,0 +1,62 @@
+CREATE TABLE IF NOT EXISTS addresses (
+	id INTEGER UNSIGNED PRIMARY KEY UNIQUE,
+	address_1 TEXT,
+	address_2 TEXT,
+	city TEXT,
+	state TEXT,
+	country TEXT,
+	zipcode INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS details (
+	id INTEGER UNSIGNED PRIMARY KEY UNIQUE,
+	paragraph TEXT
+);
+
+CREATE TABLE IF NOT EXISTS sites (
+    id INTEGER UNSIGNED PRIMARY KEY UNIQUE,
+    site TEXT
+);
+
+CREATE TABLE IF NOT EXISTS infos (
+	id INTEGER UNSIGNED PRIMARY KEY UNIQUE,
+	exec_coahing BIT NOT NULL,
+	team_develop BIT NOT NULL,
+	more_other BIT NOT NULL,
+	c_suite BIT NOT NULL,
+	hr_rep BIT NOT NULL,
+	position BIT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS dates (
+	id INTEGER UNSIGNED PRIMARY KEY UNIQUE,
+	stamp TIMESTAMP NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS companies (
+	id INTEGER UNSIGNED PRIMARY KEY UNIQUE,
+	company_name TEXT NOT NULL,
+	company_size TEXT NOT NULL,
+	phone TEXT NOT NULL,
+	email TEXT NOT NULL,
+	site TEXT,
+	address_id INTEGER NOT NULL,
+	detail_id INTEGER NOT NULL,
+	site_id INTEGER NOT NULL,
+	FOREIGN KEY(address_id) REFERENCES addresses(id),
+	FOREIGN KEY(detail_id) REFERENCES details(id),
+	FOREIGN KEY(site_id) REFERENCES sites(id)
+);
+
+CREATE TABLE IF NOT EXISTS submissions (
+	id INTEGER UNSIGNED PRIMARY KEY UNIQUE,
+	first_name TEXT NOT NULL,
+	last_name TEXT NOT NULL,
+	title TEXT NOT NULL,
+	company_id INTEGER NOT NULL,
+	info_id INTEGER NOT NULL,
+	date_id INTEGER NOT NULL,
+	FOREIGN KEY(company_id) REFERENCES companies(id),
+	FOREIGN KEY(info_id) REFERENCES infos(id),
+	FOREIGN KEY(date_id) REFERENCES dates(id)
+);
