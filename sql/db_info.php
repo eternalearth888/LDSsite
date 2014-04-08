8ca8e0b55a78627c03b4b6aa14593f2fbf0d1605
Maria Deslis <mdeslis@gmail.com>
2014-04-08 00:57:58 -0600

    able to select from database

---
+++ sql/db_info.php
@@ -0,0 +1,156 @@
+<?php
+	/* Create a new mysqli object with database connection parameters */
+   @ $db = new mysqli('localhost', 'root', '', 'ldsexecu_submissions');
+
+   if(mysqli_connect_errno()) {
+      echo "Connection Failed: " . mysqli_connect_errno();
+      exit();
+   }
+?>
+<?php
+	$id=$_GET['id'];
+	if ($db) {
+		$stmt = $db->prepare("SELECT * FROM submissions WHERE id=?");
+
+		if ($stmt->bind_param("i", $id)) {
+		   echo "Query success!";
+		} else {
+			echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+		}
+ 		 /* Bind results */
+		$stmt->bind_result( $id,
+					$address1,
+					$address2,
+					$city,
+					$state,
+					$country,
+					$code,
+					$details,
+					$site,
+					$coachV,
+					$teamV,
+					$infoV,
+					$suiteV,
+					$repV,
+					$posV,
+					$company,
+					$orgSize,
+					$phone,
+					$email,
+					$fname,
+					$lname,
+					$title,
+					$dateStamp
+				);
+
+		if (!$stmt->execute()) {
+		    echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+		} else {
+		    /* Fetch the value */
+		    $stmt->fetch();
+		}
+?>
+	<table align="center">
+			<thead align="center">
+				<tr>
+					<td colspan="9">Personal Info</td>
+				</tr>
+			</thead>
+			<thead class="displayHead2">
+				<tr>
+					<td colspan="2" align="center">Submitted</td>
+					<td>Title</td>
+					<td>First Name</td>
+					<td>Last Name</td>
+					<td>Phone</td>
+					<td colspan="3" align="right">Email</td>
+				</tr>
+			</thead>
+			<tr>
+<?php
+	echo '<td colspan="2" align="center">'.$dateStamp.'</td>';
+	echo '<td>'.$title.'</td>';
+	echo '<td>'.$fname.'</td>';
+	echo '<td>'.$lname.'</td>';
+	echo '<td>'.$phone.'</td>';
+	echo '<td colspan="3" align="right">'.$email.'</td>';
+?>
+	</tr>
+		<thead align="center" >
+			<tr>
+				<td colspan="9">Company Info</td>
+			</tr>
+		</thead>
+		<thead class="displayHead2">
+			<tr>
+				<td>Company</td>
+				<td>Size</td>
+				<td>Website</td>
+				<td>Add. 1</td>
+				<td>Add. 2</td>
+				<td>City</td>
+				<td>State</td>
+				<td>Country</td>
+				<td>ZipCode</td>
+			</tr>
+		</thead>
+		<tr>
+		<?php
+	echo '<td >'.$company.'</td>';
+	echo '<td >'.$orgSize.'</td>';
+	echo '<td >'.$site.'</td>';
+	echo '<td >'.$address1.'</td>';
+	echo '<td >'.$address2.'</td>';
+	echo '<td >'.$city.'</td>';
+	echo '<td >'.$state.'</td>';
+	echo '<td >'.$country.'</td>';
+	echo '<td >'.$code.'</td>';
+?>
+	</tr>
+	<thead align="center" >
+		<tr>
+			<td colspan="4">Seeking</td>
+			<td colspan="4">Position</td>
+		</tr>
+	</thead>
+	<thead align="center" >
+		<tr>
+			<td colspan="2">Executive Coaching</td>
+			<td>Team Development</td>
+			<td>Other</td>
+			<td>C-Suite Exec</td>
+			<td colspan="3">HR-Representative</td>
+			<td>Other</td>
+		</tr>
+	</thead>
+	<tr>
+<?php
+	echo '<td  colspan="2">'.$coachV.'</td>';
+	echo '<td >'.$teamV.'</td>';
+	echo '<td >'.$infoV.'</td>';
+	echo '<td >'.$suiteV.'</td>';
+	echo '<td  colspan="3">'.$repV.'</td>';
+	echo '<td >'.$posV.'</td>';
+?>
+	</tr>
+	<thead align="center">
+	</tr>
+		<td colspan="9">Details</td>
+	</tr>
+	</thead>
+	<tr>
+<?php
+	echo '<td colspan="9">'.$details.'</td>';
+?>
+	</tr>
+</table>
+<?php
+			    /* Close statement */
+		    	$stmt->close();
+	} else {
+		echo '<h1>DATABASE NOT FOUND: CONTACT WEBMASTER</h1>';
+	}			
+?>	
+<?php
+	$db->close();
+?>
--- sql/db_view.php
+++ sql/db_view.php
@@ -16,114 +16,47 @@
 			if (is_null($num_results)) {
 				echo '<h2>'.'No data in the database. Please come and try again later'.'</h2>';
 			} else {
-				echo '<h2>'.'Database results:'.'</h2>';
-				for ($i = 0; $i < $num_results; $i++) {
-					$row = $result->fetch_assoc();
+				echo '<h1>'.'Database results:'.'</h1>';
 ?>
+
 	<table id="displayTable">
-		<thead class="displayHead1">
-			<tr>
-				<td colspan="9">Personal Info</td>
-			</tr>
-		</thead>
-		<thead class="displayHead2">
-			<tr>
-				<td>ID</td>
-				<td colspan="2">Submitted</td>
-				<td>Title</td>
-				<td>First Name</td>
-				<td>Last Name</td>
-				<td>Phone</td>
-				<td colspan="2">Email</td>
-			</tr>
-		</thead>
-		<tr>
-<?php
-	echo '<td class="cell">'.$row['id'].'</td>';
-	echo '<td class="cell" colspan="2">'.$row['stamp'].'</td>';
-	echo '<td class="cell">'.$row['title'].'</td>';
-	echo '<td class="cell">'.$row['first_name'].'</td>';
-	echo '<td class="cell">'.$row['last_name'].'</td>';
-	echo '<td class="cell">'.$row['phone'].'</td>';
-	echo '<td class="cell" colspan="2">'.$row['email'].'</td>';
-?>
-	</tr>
 	<thead class="displayHead1">
 		<tr>
-			<td colspan="9">Company Info</td>
+			<td align="right">ID</td>
+			<td colspan="2" align="center">Submitted</td>
+			<td align="left">Title</td>
+			<td align="left">First Name</td>
+			<td align="left">Last Name</td>
+			<td align="right">Phone</td>
+			<td colspan="2" align="right">Email</td>
+			<td align="center"> More Info</td>
 		</tr>
 	</thead>
-	<thead class="displayHead2">
+<?php
+		for ($i = 0; $i < $num_results; $i++) {
+			$row = $result->fetch_assoc();
+?>
+
 		<tr>
-			<td>Company</td>
-			<td>Size</td>
-			<td>Website</td>
-			<td>Add. 1</td>
-			<td>Add. 2</td>
-			<td>City</td>
-			<td>State</td>
-			<td>Country</td>
-			<td>ZipCode</td>
-		</tr>
-	</thead>
-	<tr>
 <?php
-	echo '<td class="cell">'.$row['company_name'].'</td>';
-	echo '<td class="cell">'.$row['company_size'].'</td>';
-	echo '<td class="cell">'.$row['site'].'</td>';
-	echo '<td class="cell">'.$row['address_1'].'</td>';
-	echo '<td class="cell">'.$row['address_2'].'</td>';
-	echo '<td class="cell">'.$row['city'].'</td>';
-	echo '<td class="cell">'.$row['state'].'</td>';
-	echo '<td class="cell">'.$row['country'].'</td>';
-	echo '<td class="cell">'.$row['zipcode'].'</td>';
+	echo '<td align="right">'.$row['id'].'</td>';
+	echo '<td colspan="2" align="center">'.$row['stamp'].'</td>';
+	echo '<td align="left">'.$row['title'].'</td>';
+	echo '<td align="left">'.$row['first_name'].'</td>';
+	echo '<td align="left">'.$row['last_name'].'</td>';
+	echo '<td align="right">'.$row['phone'].'</td>';
+	echo '<td colspan="2" align="right">'.$row['email'].'</td>';
+	echo '<td colspan="2" align="center"><a href="sql/db_info.php?id='.$row['id'].'" target="_blank">View</a></td>';
 ?>
 	</tr>
-	<thead class="displayHead1">
-		<tr>
-			<td colspan="4">Seeking</td>
-			<td colspan="5">Position</td>
-		</tr>
-	</thead>
-	<thead class="displayHead2">
-		<tr>
-			<td colspan="2">Executive Coaching</td>
-			<td>Team Development</td>
-			<td>Other</td>
-			<td>C-Suite Exec</td>
-			<td colspan="3">HR-Representative</td>
-			<td>Other</td>
-		</tr>
-	</thead>
-	<tr>
-<?php
-	echo '<td class="cell" colspan="2">'.$row['exec_coaching'].'</td>';
-	echo '<td class="cell">'.$row['team_develop'].'</td>';
-	echo '<td class="cell">'.$row['more_other'].'</td>';
-	echo '<td class="cell">'.$row['c_suite'].'</td>';
-	echo '<td class="cell" colspan="3">'.$row['hr_rep'].'</td>';
-	echo '<td class="cell">'.$row['position'].'</td>';
-?>
-	</tr>
-	<thead class="displayHead1">
-	</tr>
-		<td colspan="9">Details</td>
-	</tr>
-	</thead>
-	<tr>
-<?php
-	echo '<td colspan="9">'.$row['paragraph'].'</td>';
-?>
-	</tr>
-</table>
-<?php
-	}
-?>
+
 <?php
 			}
+?>
+</table>
+<?php
 		}
-	} else {
-		echo "Database NOT Found ";
 	}
+}
 	$db->close();
 ?>
